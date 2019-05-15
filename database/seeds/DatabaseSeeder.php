09a016d049bd932136c113399fc5fe7fb6bb8958
Charlie Olsson <25174519+olssoncharlie@users.noreply.github.com>
2019-05-15 20:39:35 +0200

    seed for herbs_stores relation

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(HerbsTableSeed::class);
         $this->call(StoresTableSeed::class);
         $this->call(ReviewsTableSeed::class);
+        $this->call(HerbsStoresTableSeed::class);
     }
 }
